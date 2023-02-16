b06a1f33550e43ff05fd87224f814c7b62a4bfef
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-16 11:55:50 +0500

    finished task 2

--- Homework task 2/Program.cs
+++ Homework task 2/Program.cs
@@ -3,5 +3,17 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Input first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
 
+if (a < b)
+{
+    Console.WriteLine("max == "+b);
+}
+else
+{
+    Console.WriteLine("max == "+a);
+}
